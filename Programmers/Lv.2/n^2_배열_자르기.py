49975aa2736d5d9332f5587223e8dcedc38ea80b
jorippppong <duelee75@gmail.com>
2024-03-18 12:58:49 +0900

    Programmers/Lv.2/n^2_배열_자르기.py - 24.03.18

---
+++ Programmers/Lv.2/n^2_배열_자르기.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/87390
+
+def solution(n, left, right):
+    answer = []
+    for i in range(left, right+1):
+        x, y = i//n, i%n
+        answer.append(max(x,y)+1)
+    return answer
