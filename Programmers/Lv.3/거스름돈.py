102a68488cbb49fc3ee41fa7c87cf7db3d03da33
jorippppong <duelee75@gmail.com>
2024-05-14 10:09:39 +0900

    Programmers/Lv.3/거스름돈.py - 24.05.14

---
+++ Programmers/Lv.3/거스름돈.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12907
+
+def solution(n, money):
+    dp = [0]*(n+1)
+    dp[0] = 1
+    for m in money:
+        for i in range(m, n+1):
+            dp[i] += dp[i-m]
+    return dp[n] % 1000000007
