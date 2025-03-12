dbd2e553a956483c6148853e487d0de971dc51f4
jorippppong <duelee75@gmail.com>
2025-03-12 20:58:04 +0900

    LC_17, LC_198 - 25.03.12

---
+++ LeetCode/Medium/LC_17.java
@@ -0,0 +1,56 @@
+package LeetCode.Medium;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+1. 아이디어 : 
+Backtracking
+
+2. 시간복잡도 : 
+(n : digits의 길이)
+O(3^n) ~ O(4^n)
+
+3. 자료구조/알고리즘 : 
+Backtracking, Map
+*/
+
+public class LC_17 {
+    class Solution {
+        private List<String> ans = new ArrayList<>();
+
+        public List<String> letterCombinations(String digits) {
+            if(digits == null || digits.isBlank()) return Collections.emptyList();
+
+            Map<Character, String> digitMap = new HashMap<>();
+            digitMap.put('2', "abc");
+            digitMap.put('3', "def");
+            digitMap.put('4', "ghi");
+            digitMap.put('5', "jkl");
+            digitMap.put('6', "mno");
+            digitMap.put('7', "pqrs");
+            digitMap.put('8', "tuv");
+            digitMap.put('9', "wxyz");
+
+            StringBuilder sb = new StringBuilder();
+            combine(digits, sb, 0, digitMap);
+            return ans;
+        }
+
+        public void combine(String digits, StringBuilder sb, int idx, Map<Character, String> digitMap){
+            if(idx == digits.length()){
+                ans.add(sb.toString());
+                return;
+            }
+            String digit = digitMap.get(digits.charAt(idx));
+            for(char c : digit.toCharArray()){
+                sb.append(c);
+                combine(digits, sb, idx+1, digitMap);
+                sb.deleteCharAt(idx);
+            }
+        }
+    }
+}
---
+++ LeetCode/Medium/LC_198.java
@@ -0,0 +1,29 @@
+package LeetCode.Medium;
+
+/*
+1. 아이디어 : 
+DP
+i번째 집을 털 경우: dp[i-2] + nums[i] (i-2번째 집까지 털고 i번째 집을 털기)
+i번째 집을 털지 않을 경우: dp[i-1] (i-1번째 집까지 털었을 때의 최대 금액)
+둘 중 최댓값을 dp[i]에 저장한다.
+
+2. 시간복잡도 : 
+O(n)
+
+3. 자료구조/알고리즘 : 
+DP, 반복문
+*/
+
+public class LC_198 {
+    class Solution {
+        public int rob(int[] nums) {
+            int len = nums.length;
+            int[] dp = new int[len+1];
+            dp[1] = nums[0];
+            for(int i=2; i<=len; i++){
+                dp[i] = Math.max(dp[i-2]+nums[i-1], dp[i-1]);
+            }        
+            return dp[len];
+        }
+    }
+}
